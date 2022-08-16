c5aca5ee4946060e4fc5e945d58078d286b983e4
singhhimansh <him.signum802@gmail.com>
2022-08-16 12:52:51 +0530

    add: aleart message | ux imroved ⚠️

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 import { info } from "autoprefixer";
+import { MdDeleteForever } from "react-icons/md";
+import { SiAddthis } from "react-icons/si";
 
 function App() {
   // hook to fetch from API
@@ -9,6 +11,9 @@ function App() {
 
   // hook for custom task inserted
   const [inputValue, setInputValue]= useState('');
+
+  // hook for alert message if input is too small
+  const [alert, setAlert] = useState(false);
 
 
   // API call and fetching data in to a dict
@@ -30,6 +35,8 @@ function App() {
       settodoByID(dataDict);
     })();
   }, []);
+
+
 
   const handleToggleChecked = (e,id)=>{
     // console.info(e.target.checked,id)
@@ -55,33 +62,39 @@ function App() {
   }
 
   const handleAddTodo =(value)=>{
+    value=value.trim();
+    if (value.length > 2) {
+      setAlert(false);
+      const newID = Math.random();
+      settodoByID((previous)=>({ 
+        ...previous,
+        [newID]:{
+          
+          id:newID,
+          title:value,
+          completed:false,
+        }
 
-    const newID = Math.random();
-    settodoByID((previous)=>({ 
-      ...previous,
-      [newID]:{
-        
-        id:newID,
-        title:value,
-        completed:false,
-      }
+      }));
 
-    }));
+      setInputValue('');
+    }
 
-    setInputValue('');
-
+    else {
+      setAlert(true)
+    }
 
   }
 
   return (
     // <div className="min-w-screen pt-10 pb-16  min-h-screen bg-gray-400" >
-      <div className=" min-w-screen flex flex-col items-center">
-        <div className="w-5/6 lg:w-1/2 m-10  bg-gray-50 shadow-lg  shadow-indigo-500/40  border-solid border-2 border-gray-300">
-          <div className="w-full flex flex-col items-center">
-            <h1 className="my-10 font-semibold text-2xl">Things To Do</h1>
+      <div className=" min-w-screen flex flex-col items-center ">
+        <div className="w-5/6 lg:w-1/2 m-10  bg-gray-50/90  shadow-sky-400/60  border-solid border-2 shadow-xl border-gray-300 rounded-md  ">
+          <div className="w-full mb-20 flex flex-col items-center ">
+            <h1 className="my-10 font-bold text-gray-800/90 text-2xl">Things To Do</h1>
             <hr className="w-11/12 md:w-3/4 border-solid border-t-2 border-gray-300" />
             {/* Return fetched data */}
-            <div className="my-5 flex flex-col w-11/12 lg:w-9/12 ">
+            <div className="my-5 flex flex-col w-11/12 lg:w-9/12 drop-shadow-lg ">
               {
                 Object.keys(todoByID).length>0 ? 
               
@@ -90,13 +103,14 @@ function App() {
 
                   return (
                     <div key={item.id} className="px-5 py-3 flex items-center gap-5">
-                      <input type="checkbox" className="cursor-pointer flex-none" id={`todo-item-${item.id}`} checked={item.completed} onChange={(e)=> handleToggleChecked(e,item.id)} />
+                      <input type="checkbox" className={`cursor-pointer flex-none accent-red-400 ${item.completed ? 'opacity-70' : '' } `} id={`todo-item-${item.id}`} checked={item.completed} onChange={(e)=> handleToggleChecked(e,item.id)} />
 
-                      <label className={` pb-1 w-11/12 flex-initial cursor-pointer ${item.completed ? 'line-through' : '' } `} htmlFor={`todo-item-${item.id}`} >{item.title}</label>
+                      <label title="Mark it complete" className={` pb-1 w-11/12 flex-initial hover:text-red-500 cursor-pointer ${item.completed ? 'line-through text-red-500 opacity-60' : '' } `} htmlFor={`todo-item-${item.id}`} >{item.title}</label>
                       
-                      <button type="button" onClick={()=>handeleDeleteTodo(item.id)} className="text-xs text-center flex-none text-white bg-red-400  border-2 border-solid border-red-200 rounded-full pb-4 w-5 h-5 hover:bg-red-500">
-                        x
+                      <button type="button" title="Delete task" onClick={()=>handeleDeleteTodo(item.id)} className="w-6 h-7">
+                        <MdDeleteForever className=" fill-red-500 "/>
                       </button>
+                      
                     </div>
                   );
                 }) : <div className="text-center text-sm italic text-gray-500"> Looks like you're absolutely free today. <br /> Why not go for a walk then! </div> 
@@ -106,14 +120,16 @@ function App() {
             
 
             {/* completed tasks counter */}
-            <div className="mt-10 text-lg font-semibold">Tasks completed : {Object.values(todoByID).filter((item)=> item.completed).length}</div>
-            <div className=" flex mt-10 mb-20 mx-6">
+            <div className="mt-10 text-lg font-semibold ">Tasks completed : {Object.values(todoByID).filter((item)=> item.completed).length}</div>
+
+            <div className={`w-5/6 md:w-4/6 relative flex mt-10  mx-6 gap-1 md:gap-2 ${ alert ? "before:content-['*Minimum_three_characters_allowed'] before:text-red-400 before:text-xs before:absolute before:-bottom-5 before:left-3" : "" }`}>
               
               {/* input manual tasks */}
-              <input type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)} className="px-4 h-10 w-3/4 md:w-80 mx-1 flex-1 border-2 border-solid border-gray-400 rounded-md" placeholder="Enter new task" name="tasktitle" id="task" />
+              <input type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)} className=" relative px-4 h-10 w-3/4 md:w-80 mx-1 flex-1 border-2  border-gray-400 focus:outline-green-400  rounded-md " placeholder="Enter new task" name="tasktitle" id="task" />
 
               {/* add task button */}
-              <button type="submit" onClick={()=>handleAddTodo(inputValue)} name="Delete the Note" className=" h-10 w-24 mx-1 text-sm text-white bg-blue-600 rounded-md shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out drop-shadow-xl"> ADD TASK</button>
+              <button type="submit" onClick={()=>handleAddTodo(inputValue)} name="Add a Note" className=" "><SiAddthis title="Add task"  className="w-9 h-9 fill-green-600 hover:fill-green-500 hover:shadow-lg focus:shadow-lg active:fill-green-700  transition duration-150 ease-in-out drop-shadow-xl" /></button>
+                
             </div>
           </div>
         </div>
